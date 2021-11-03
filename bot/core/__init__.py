cd2d28a69ca47c578749d2191823040b3e5cc1bd
rmenai <rami.menai@outlook.com>
2021-11-03 19:05:21 +0300

    Add constants file

--- bot/core/__init__.py
+++ bot/core/__init__.py
@@ -1,1 +1,2 @@
 from bot.core.config import settings
+from bot.core.constants import constants
---
+++ bot/core/constants.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+
+class Colours(BaseModel):
+    """Colour codes."""
+
+    blue = 0x0279FD
+    bright_green = 0x01D277
+    dark_green = 0x1F8B4C
+    orange = 0xE67E22
+    pink = 0xCF84E0
+    purple = 0xB734EB
+    soft_green = 0x68C290
+    soft_orange = 0xF9CB54
+    soft_red = 0xCD6D6D
+    yellow = 0xF9F586
+    python_blue = 0x4B8BBE
+    python_yellow = 0xFFD43B
+    grass_green = 0x66FF00
+    gold = 0xE6C200
+
+
+class Constants(BaseModel):
+    """The app constants."""
+
+    colours: Colours = Colours()
+
+
+constants = Constants()
