78ed6660178e28d17fc0b090c3f7a93db021276e
rmenai <rami.menai@outlook.com>
2021-11-09 22:00:25 +0300

    Change logging format

--- bot/__init__.py
+++ bot/__init__.py
@@ -5,51 +5,51 @@ from pathlib import Path
 import arrow
 from bot.core import settings
 
-# Set timestamp of when execution started (approximately)
+# Set timestamp of when execution started.
 start_time = arrow.utcnow()
 
-# Set up file logging
-log_dir = Path("bot/logs")
+# Set up file logging.
+log_dir = Path(f"{__name__}/logs")
 log_file = log_dir / f"{arrow.utcnow().strftime('%d-%m-%Y')}.log"
 os.makedirs(log_dir, exist_ok=True)
 
-# File handler rotates logs every 5 MB
+# File handler rotates logs every 5 MB.
 file_handler = logging.handlers.RotatingFileHandler(
     log_file, maxBytes=5 * (2 ** 20), backupCount=10, encoding="utf-8",
 )
 file_handler.setLevel(logging.DEBUG)
 
-# Console handler prints to terminal
+# Console handler prints to terminal.
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG if settings.debug else logging.INFO)
+
+# Format configuration.
+fmt = "%(asctime)s - %(name)s %(levelname)s: %(message)s"
+datefmt = "%H:%M:%S"
 
 # Add colors for logging if available.
 try:
     from colorlog import ColoredFormatter
 
     console_handler.setFormatter(
-        ColoredFormatter(fmt="%(log_color)s%(asctime)s - %(name)s %(levelname)s: %(message)s", datefmt="%D %H:%M:%S"))
+        ColoredFormatter(fmt=f"%(log_color)s{fmt}", datefmt=datefmt))
 except ModuleNotFoundError:
     pass
 
-# Remove old loggers, if any
+# Remove old loggers, if any.
 root = logging.getLogger()
 if root.handlers:
     for handler in root.handlers:
         root.removeHandler(handler)
 
-# Silence irrelevant loggers
+# Silence irrelevant loggers.
 logging.getLogger("discord").setLevel(logging.ERROR)
 logging.getLogger("asyncio").setLevel(logging.ERROR)
 
-# Setup new logging configuration
+# Setup new logging configuration.
 logging.basicConfig(
-    format="%(asctime)s - %(name)s %(levelname)s: %(message)s",
-    datefmt="%D %H:%M:%S",
+    format=fmt,
+    datefmt=datefmt,
     level=logging.DEBUG,
     handlers=[console_handler, file_handler]
 )
-
-log = logging.getLogger()
-
-log.debug("Logging initialization complete")
