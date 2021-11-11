3c5b70ea41f3d6bfb0d4471b86ad4a8e43f15f7e
rmenai <rami.menai@outlook.com>
2021-11-11 23:29:50 +0300

    Remove useless loaded_extensions variable

--- bot/utils/extensions.py
+++ bot/utils/extensions.py
@@ -4,7 +4,6 @@ import pkgutil
 from typing import Iterator, NoReturn
 
 from bot import exts
-from bot.bot import bot
 
 
 def unqualify(name: str) -> str:
@@ -34,4 +33,3 @@ def walk_extensions() -> Iterator[str]:
 
 # Required for the core.extensions cog.
 EXTENSIONS = frozenset(walk_extensions())
-loaded_extensions = bot.extensions
