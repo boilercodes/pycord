5b18a08c1505d2d42cc64dc2c7c306bb405f91ce
rmenai <rami.menai@outlook.com>
2021-11-12 23:00:27 +0300

    Ignore coverage from logging functions

--- bot/bot.py
+++ bot/bot.py
@@ -26,12 +26,12 @@ class Bot(commands.Bot, ABC):
             trace_configs=[trace_config]
         )
 
-    async def on_ready(self) -> None:
+    async def on_ready(self) -> None:  # pragma: no cover
         """Triggered when the bot is ready."""
         name = f"{self.user} (ID: {self.user.id})"
         log.info(f"Started bot as {name}")
 
-    async def on_interaction(self, interaction: Interaction) -> None:
+    async def on_interaction(self, interaction: Interaction) -> None:  # pragma: no cover
         """Log whenever a command is used."""
         arguments = [option['value'] for option in interaction.data.get("options", "")]
         command = f"{interaction.data['name']} {arguments if arguments else ''}".rstrip()
