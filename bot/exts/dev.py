5e33a3cdeb5671ed56e89dbd8cdb999de2b92c37
rmenai <rami.menai@outlook.com>
2021-11-12 20:02:37 +0300

    Add a dev cog

---
+++ bot/exts/dev.py
@@ -0,0 +1,26 @@
+import logging
+
+from discord.commands import ApplicationContext, slash_command
+from discord.ext import commands
+
+from bot import settings
+from bot.bot import Bot
+
+log = logging.getLogger(__name__)
+
+
+class Dev(commands.Cog):
+    """Test different commands and pycord features."""
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @slash_command(guild_ids=settings.dev_guild_ids)
+    async def test(self, ctx: ApplicationContext) -> None:
+        """Usage for testing purposes."""
+        await ctx.defer()
+
+
+def setup(bot: Bot) -> None:
+    """Loads the Dev cog."""
+    bot.add_cog(Dev(bot))
