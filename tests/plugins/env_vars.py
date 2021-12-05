aa5740f9678c54dd66cf3075d5a6c8d7ac9894ca
Rami Menai <rami.menai@outlook.com>
2021-12-05 20:07:58 +0300

    Replace ' with " for convention

--- tests/plugins/env_vars.py
+++ tests/plugins/env_vars.py
@@ -5,5 +5,5 @@ import pytest
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_load_initial_conftests():
-    os.environ['BOT_TOKEN'] = "ODk3MTVyNDO0MDAxODE0NTC4.YWRgYg.hqWNRy2vEoc8feoNqR0ubBCYwxo"
-    os.environ['GUILD_IDS'] = "[776477173123907605,831216309592653835]"
+    os.environ["BOT_TOKEN"] = "ODk3MTVyNDO0MDAxODE0NTC4.YWRgYg.hqWNRy2vEoc8feoNqR0ubBCYwxo"
+    os.environ["GUILD_IDS"] = "[776477173123907605,831216309592653835]"
